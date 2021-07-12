4c1ab76fc42d79938375d105c938b4e4a2b1759f
takezawatsuyoshi <takezawatsuyoshi777@gmail.com>
2021-07-12 15:53:35 +0900

    add new file

--- python/myproject/settings.py
+++ python/myproject/settings.py
@@ -73,23 +73,23 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'first',
-#         'USER': 'admin',
-#         'PASSWORD': 't19871027',
-#         'HOST': 'database-1.cqdaaoulhur2.ap-northeast-1.rds.amazonaws.com',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'first',
+        'USER': 'admin',
+        'PASSWORD': 't19871027',
+        'HOST': 'database-1.cqdaaoulhur2.ap-northeast-1.rds.amazonaws.com',
+        'PORT': '3306',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
