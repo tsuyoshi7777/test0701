f6a745475f6f82a43d84b09b226ed827649e5fdd
takezawatsuyoshi <takezawatsuyoshi777@gmail.com>
2022-05-19 20:32:00 +0900

    add new file

--- nginx/vue.config.js
+++ nginx/vue.config.js
@@ -1,7 +1,7 @@
 const BundleTracker = require('webpack-bundle-tracker')
 
 // Change this to match the path to your files in production (could be S3, CloudFront, etc.)
-const DEPLOYMENT_PATH = '/static/dist/'
+const DEPLOYMENT_PATH = './dist/'
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? DEPLOYMENT_PATH : 'http://localhost:8080/',
